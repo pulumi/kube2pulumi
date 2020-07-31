668d8c9f45f000020f00996db044e92fa18eb069
sashu-shankar <sashws@cs.washington.edu>
2020-07-30 18:03:52 -0700

    build temporary .pp file with proper file contents written in.

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/pulumi/kube2pulumi/yaml2pcl"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var testData = `
-
 apiVersion: v1
 kind: Namespace
 metadata:
-	name: foo
+  name: foo
 
 `
 
@@ -21,7 +23,29 @@ func main() {
 	result, err := yaml2pcl.Convert([]byte(testData))
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(result)
+	}
+
+	tempFile, err := ioutil.TempFile("", "test-*.pp")
+	if err != nil {
+		panic(err)
+	}
+
+	// ensure to clean up the file
+	defer os.Remove(tempFile.Name())
+	fmt.Println("created the file: ", tempFile.Name())
+
+	// Write to the file
+	text := []byte(result)
+	if _, err = tempFile.Write(text); err != nil {
+		log.Fatal("Failed to write to temporary file", err)
+	}
+
+	// test printing out file contents
+	out, err := ioutil.ReadFile(tempFile.Name())
+	fmt.Println(string(out))
+
+	// Close the file
+	if err := tempFile.Close(); err != nil {
+		log.Fatal(err)
 	}
 }
