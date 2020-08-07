fbbf8ea5804c95f3250b02a2488ac1eb723ae75c
sashu-shankar <67289341+sashu-shankar@users.noreply.github.com>
2020-08-07 13:30:25 -0700

    added comment fixes and "all" command in CLI  (#24)
    * added comment fixes and "all" command in CLI to convert yaml to all supported languages
    
    * removed generated files
    
    * updated -f to read in a file instad of manifest and rebased from master
    
    * updated gitignore to ignore generated files in tests

---
+++ cmd/kube2pulumi/all/cli.go
@@ -0,0 +1,45 @@
+package all
+
+import (
+	"fmt"
+	"github.com/pulumi/kube2pulumi/cmd/kube2pulumi/util"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func Command() *cobra.Command {
+	command := &cobra.Command{
+		Use:  "all",
+		Long: "convert k8s yaml to all supported languages",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dirPath := viper.GetString("directory")
+			filePath := viper.GetString("file")
+
+			python, err := util.RunConversion(dirPath, filePath, "python")
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Conversion successful! Generated File: %s.py\n", python)
+
+			nodejs, err := util.RunConversion(dirPath, filePath, "nodejs")
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Conversion successful! Generated File: %s.js\n", nodejs)
+
+			dotnet, err := util.RunConversion(dirPath, filePath, "dotnet")
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Conversion successful! Generated File: %s.cs\n", dotnet)
+
+			golang, err := util.RunConversion(dirPath, filePath, "go")
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Conversion successful! Generated File: %s.go\n", golang)
+			return nil
+		}}
+
+	return command
+}
