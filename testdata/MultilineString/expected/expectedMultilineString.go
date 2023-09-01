d61eb724e3e15e0ed162ed9ae469e3510a0679a6
Ian Wahbe <ian@wahbe.com>
2023-09-01 11:36:39 -0700

    PULUMI_ACCEPT=true go test ./...

--- testdata/MultilineString/expected/expectedMultilineString.go
+++ testdata/MultilineString/expected/expectedMultilineString.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
