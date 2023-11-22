664e08407058e394f1501b9bc5bcdc5d553e82b0
Eron Wright <eron@pulumi.com>
2023-11-21 20:13:24 -0800

    avoid using String function, use Value accessors, new cilium example
    (cherry picked from commit 6b52469b976341570606dd298f42a89b6d775e68)

--- pkg/yaml2pcl/yaml2pcl.go
+++ pkg/yaml2pcl/yaml2pcl.go
@@ -121,10 +121,16 @@ func getHeader(nodes []ast.Node) (string, hcl.Diagnostic) {
 	for _, node := range nodes {
 		if mapValNode, ok := node.(*ast.MappingValueNode); ok {
 			if mapValNode.Key.String() == "apiVersion" {
-				comment := mapValNode.Value.GetComment()
-				mapValNode.Value.SetComment(nil)
-				apiVersion = trimQuotes(mapValNode.Value.String())
-				mapValNode.Value.SetComment(comment)
+				val, ok := mapValNode.Value.(*ast.StringNode)
+				if !ok {
+					return "", hcl.Diagnostic{
+						Severity: hcl.DiagnosticSeverity(1),
+						Summary:  "malformed yaml: apiVersion value is wrong type",
+						Detail:   "apiVersion field for the resource needs to be a string",
+					}
+				}
+
+				apiVersion = trimQuotes(val.Value)
 				break
 			}
 		}
@@ -178,32 +184,36 @@ func resourceName(nodes []ast.Node) (string, string) {
 	for _, node := range nodes {
 		if mapValNode, ok := node.(*ast.MappingValueNode); ok {
 			if len(kind) == 0 && mapValNode.Key.String() == "kind" {
-				comment := mapValNode.Value.GetComment()
-				mapValNode.Value.SetComment(nil)
-				kind = mapValNode.Value.String()
-				mapValNode.Value.SetComment(comment)
+				val, ok := mapValNode.Value.(*ast.StringNode)
+				if !ok {
+					return "", ""
+				}
+				kind = val.Value
 				continue
 			}
 			if mapValNode.Key.String() == "metadata" {
 				if mapValNode.Value.Type() == ast.StringType {
-					comment := mapValNode.Value.GetComment()
-					mapValNode.Value.SetComment(nil)
-					name = mapValNode.Value.String()
-					mapValNode.Value.SetComment(comment)
+					val, ok := mapValNode.Value.(*ast.StringNode)
+					if !ok {
+						return "", ""
+					}
+					name = val.Value
 					continue
 				} else {
 					for _, inner := range ast.Filter(ast.MappingValueType, node) {
 						if innerMvNode, ok := inner.(*ast.MappingValueNode); ok {
 							if innerMvNode.Key.String() == "name" {
-								comment := innerMvNode.Value.GetComment()
-								innerMvNode.Value.SetComment(nil)
-								name = innerMvNode.Value.String()
-								innerMvNode.Value.SetComment(comment)
+								val, ok := innerMvNode.Value.(*ast.StringNode)
+								if !ok {
+									return "", ""
+								}
+								name = val.Value
 							} else if innerMvNode.Key.String() == "namespace" {
-								comment := innerMvNode.Value.GetComment()
-								innerMvNode.Value.SetComment(nil)
-								ns = innerMvNode.Value.String()
-								innerMvNode.Value.SetComment(comment)
+								val, ok := innerMvNode.Value.(*ast.StringNode)
+								if !ok {
+									return "", ""
+								}
+								ns = val.Value
 							}
 						}
 					}
@@ -237,13 +247,9 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 	tk := node.GetToken()
 	switch n := node.(type) {
 	case *ast.LiteralNode:
-		// Grab any trailing comment, but set the comment to nil so that it doesn't get pasted into
-		// the string representation of the literal as well.
-		comment := addComment(node)
-		node.SetComment(nil)
+		multLine := n.Value.Value
 
-		multLine := node.String()
-		multLine = strings.TrimPrefix(multLine, "|")
+		// note: PCL heredoc strings must have a trialing newline
 		multLine = strings.TrimSpace(multLine)
 		// Escape any ${} interpolation sequences.
 		multLine = strings.ReplaceAll(multLine, "${", "$${")
@@ -252,6 +258,7 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 			return err
 		}
 
+		comment := addComment(node)
 		if comment != "" {
 			_, err = fmt.Fprintf(totalPCL, "%s", comment)
 			if err != nil {
@@ -259,7 +266,7 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 			}
 		}
 	case *ast.NullNode:
-		_, err = fmt.Fprintf(totalPCL, "%s\n", node)
+		_, err = fmt.Fprintf(totalPCL, "null\n")
 		if err != nil {
 			return err
 		}
@@ -271,7 +278,7 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 			}
 		}
 	case *ast.IntegerNode:
-		_, err = fmt.Fprintf(totalPCL, "%s\n", node)
+		_, err = fmt.Fprintf(totalPCL, "%v\n", n.Value)
 		if err != nil {
 			return err
 		}
@@ -283,7 +290,7 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 			}
 		}
 	case *ast.FloatNode:
-		_, err = fmt.Fprintf(totalPCL, "%s\n", node)
+		_, err = fmt.Fprintf(totalPCL, "%v\n", n.Value)
 		if err != nil {
 			return err
 		}
@@ -296,22 +303,9 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 		}
 	case *ast.StringNode:
 		if tk.Next == nil || tk.Next.Value != ":" {
-			comment := addComment(node)
-			node.SetComment(nil)
-			s := n.String()
-			// Remove leading quote if present.
-			if len(s) > 0 && (s[0] == '"' || s[0] == '\'') {
-				s = s[1:]
-			}
-			// Remove trailing quote if present unless it is escaped.
-			if len(s) > 0 && (s[len(s)-1] == '"' || s[len(s)-1] == '\'') {
-				if len(s) == 1 {
-					s = ""
-				}
-				if len(s) > 1 && s[len(s)-2] != '\\' {
-					s = s[:len(s)-1]
-				}
-			}
+			s := n.Value
+
+			// Escape any ${} interpolation sequences.
 			s = strings.ReplaceAll(s, "${", "$${")
 			strVal := fmt.Sprintf("%q%s", s, suffix)
 			_, err = fmt.Fprintf(totalPCL, "%s\n", strVal)
@@ -319,6 +313,7 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 				return err
 			}
 			// add comments on the same line
+			comment := addComment(node)
 			if comment != "" {
 				_, err = fmt.Fprintf(totalPCL, "%s", comment)
 				if err != nil {
@@ -328,7 +323,7 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 		}
 	case *ast.MergeKeyNode:
 	case *ast.BoolNode:
-		_, err = fmt.Fprintf(totalPCL, "%s\n", node)
+		_, err = fmt.Fprintf(totalPCL, "%v\n", n.Value)
 		if err != nil {
 			return err
 		}
@@ -342,16 +337,9 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 	case *ast.InfinityNode:
 	case *ast.NanNode:
 	case *ast.TagNode:
-		_, err = fmt.Fprintf(totalPCL, "%s\n", node)
+		err = walkToPCL(v, n.Value, totalPCL, "")
 		if err != nil {
 			return err
-		}
-		comment := addComment(node)
-		if comment != "" {
-			_, err = fmt.Fprintf(totalPCL, "%s", comment)
-			if err != nil {
-				return err
-			}
 		}
 	case *ast.DocumentNode:
 		comment := addComment(node)
@@ -368,7 +356,6 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 	case *ast.MappingNode:
 		_, err = fmt.Fprintf(totalPCL, "%s\n", "{")
 		comment := addComment(node)
-		node.SetComment(nil)
 		if comment != "" {
 			_, err = fmt.Fprintf(totalPCL, "%s", comment)
 			if err != nil {
@@ -390,7 +377,6 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 		}
 	case *ast.MappingKeyNode:
 		comment := addComment(node)
-		node.SetComment(nil)
 		if comment != "" {
 			_, err = fmt.Fprintf(totalPCL, "%s", comment)
 			if err != nil {
@@ -403,19 +389,19 @@ func walkToPCL(v Visitor, node ast.Node, totalPCL io.Writer, suffix string) erro
 		}
 	case *ast.MappingValueNode:
 		comment := addComment(node)
-		node.SetComment(nil)
 		if comment != "" {
 			_, err = fmt.Fprintf(totalPCL, "%s", comment)
 			if err != nil {
 				return err
 			}
 		}
-		key := n.Key.String()
-		// trim surrounding quotations if there
-		if len(key) >= 2 {
-			if key[0] == '"' && key[len(key)-1] == '"' {
-				key = key[1 : len(key)-1]
-			}
+		var key string
+		switch nk := n.Key.(type) {
+		case ast.ScalarNode:
+			key = fmt.Sprintf("%v", nk.GetValue())
+		default:
+			return fmt.Errorf(fmt.Sprintf("unexpected key type: %T\n Please file an issue with the YAML input so we"+
+				"can take a look: https://github.com/pulumi/kube2pulumi/issues/new", n.Key))
 		}
 
 		if !hclsyntax.ValidIdentifier(key) {
@@ -516,10 +502,12 @@ func addComment(node ast.Node) string {
 	/**
 	check for comments here in order to add to the PCL string
 	*/
-	if comment := node.GetComment(); comment != nil {
-		commentVal := strings.TrimSpace(comment.String())
-		if !strings.HasPrefix(commentVal, "#") {
-			commentVal = fmt.Sprintf("# %s", commentVal)
+	if comments := node.GetComment(); comments != nil {
+		commentVal := ""
+		for _, line := range comments.Comments {
+			commentVal = fmt.Sprintf("# %s", line.Token.Value)
+			// TODO handle multi-line comments
+			break
 		}
 		return fmt.Sprintf("%s\n", commentVal)
 	}
